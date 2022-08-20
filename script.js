412ef94b187db0617e94baece755fbcbfa6c93be
mahmoudaeldieb <36247323+mahmoudaeldieb@users.noreply.github.com>
2022-08-20 17:58:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const newDate = new Date('aug 13 22 20:11:00').getTime()
+const countdown = setInterval(() => {
+
+  const date = new Date().getTime()
+  const diff = newDate - date
+
+  const month = Math.floor((diff % (1000 * 60 * 60 * 24 * (365.25 / 12) * 365)) / (1000 * 60 * 60 * 24 * (365.25 / 12)))
+  const days = Math.floor(diff % (1000 * 60 * 60 * 24 * (365.25 / 12)) / (1000 * 60 * 60 * 24))
+  const hours = Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000)
+
+  document.querySelector(".seconds").innerHTML = seconds < 10 ? '0' + seconds : seconds
+  document.querySelector(".minutes").innerHTML = minutes < 10 ? '0' + minutes : minutes
+  document.querySelector(".hours").innerHTML = hours < 10 ? '0' + hours : hours
+  document.querySelector(".days").innerHTML = days < 10 ? '0' + days : days
+  // document.querySelector(".months").innerHTML = month < 10 ? '0' + month : month
+
+  if (diff < 0) {
+    clearInterval(countdown);
+    // document.querySelector(".countdown").innerHTML = 'Happy Birthday Ahmed'
+    document.querySelector(".seconds").innerHTML = 0;
+    document.querySelector(".minutes").innerHTML = 0;
+    document.querySelector(".hours").innerHTML = 0;
+    document.querySelector(".days").innerHTML = 0;
+    document.querySelector(".months").innerHTML = 0;
+  }
+
+}, 1000);
+// clearInterval(countdown);
